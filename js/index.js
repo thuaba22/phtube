30d84c2e00efd74e3230eb53cf1a7102b1e243ea
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-09-01 22:11:25 +0600

    card showing completed and posted time added

--- js/index.js
+++ js/index.js
@@ -2,13 +2,89 @@ const handleCatagory = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
     const tabContainer = document.getElementById('tab-container');
+    
     data.data.forEach((category) => {
        const div = document.createElement('div');
        div.innerHTML = `
-       <button onclick="handleLoadVideos('${category.category_id}')" class="btn bg-[#25252533] hover:bg-[#FF1F3D] hover:text-white text-[#252525] font-medium text-lg normal-case rounded-[4px]">${category.category}</button>
+       <button onclick="handleLoadVideos('${category.category_id}')" class="btn  bg-[#25252533] focus:outline-none focus:ring focus:bg-[#FF1F3D] focus:text-white focus:ring-[#FF1F3D]  text-[#252525] font-medium text-lg normal-case rounded-[4px]">${category.category}</button>
        `;
        tabContainer.appendChild(div);
+       
     });
     
    };
+   const handleLoadVideos = async(categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+    
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = '';
+   if(data.data.length === 0){
+    const noVideoDiv = document.createElement('div');
+    noVideoDiv.innerHTML = `
+    
+        <div class="grid grid-cols-1 col-span-4 justify-center items-center">
+        <figure>
+        <img class="mx-auto" src="./images/Icon.png" alt="">
+        <figcaption>
+        <h1 class="text-2xl text-center font-bold">Oops!! Sorry, There is no content here</h1>
+        </figcaption>
+        </figure>
+          
+        </div>
+    
+    
+    `;
+    cardContainer.classList.remove('grid');
+    cardContainer.appendChild(noVideoDiv);
+
+   }
+
+   else{
+    data.data.forEach((videos) => {
+        const div = document.createElement('div');
+        const postedTime = videos.others?.posted_date;
+        const hour = Math.floor(postedTime/3600);
+        const min = Math.floor((postedTime%3600) / 60);
+        div.innerHTML = `
+        <div class="card bg-base-100 shadow-xl">
+                    <figure class="relative"><img class="h-[200px] w-full" src="${videos.thumbnail}" alt="Shoes" />
+                    ${videos.others?.posted_date ? `<div class="bg-[#171717] absolute bottom-3 right-3 rounded-[4px]">
+                    <p class="text-white text-[10px] p-1">${hour} hr ${min} min ago</p>
+                    </div>` : ''}
+                    
+                    </figure>
+                    
+                    <div class="card-body flex flex-row">
+                   <div>
+                   <img
+                   class="h-[40px] w-[40px] rounded-full"  src="${videos.authors[0]?.profile_picture}"
+                   />
+                  </div>
+                  <div>
+                     <h3 class="card-title text-[#171717] font-bold text-sm">${videos.title}</h6>
+                     <div class="flex flex-row items-center">
+                    <div>
+                    <small>${videos.authors[0]?.profile_name}</small>
+                    </div>
+                    <div>
+                    <p>${videos.authors[0]?.verified? ' <img src="./images/icons8-verified-badge-16.png" alt="">' :''}</p>
+                    </div>
+                    </div>
+                    <small>${videos.others?.views} views</small>
+                  </div>
+                   </div>
+                  </div>
+                      
+        `;
+        cardContainer.classList.add('grid');
+        cardContainer.appendChild(div);
+    
+    
+        });
+   }
+
+
+   }
    handleCatagory();
+   handleLoadVideos('1000');
